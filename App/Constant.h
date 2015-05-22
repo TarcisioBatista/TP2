85b6c551cbec5365814b861783a7c9bd19aa2327
JessicaFFerreira <jessicafferreira11@gmail.com>
2015-05-22 17:31:09 -0300

    Frame principal funcionando

--- App/Constant.h
+++ App/Constant.h
@@ -6,12 +6,20 @@
 //Esta classe possui diversas constantes utilizadas no programa
 
 //Strings
+
+//Nome da frame principal
 wxString nameMain = "TP2";
 
+
 //Pontos
+
+//Posição inicial da frame
 wxPoint posMain = wxPoint (50,50);
 
+
 //Tamanhos
+
+//Tamanho da frame principal
  wxSize sizeMain = wxSize(900,700);
 
 #endif
--- App/MainApp.cpp
+++ App/MainApp.cpp
@@ -1,10 +1,14 @@
 #include "MainApp.h"
 
-IMPLEMENT_APP(MainApp)  
-
+IMPLEMENT_APP(MainApp)
+  
+//O método OnInit cria uma frame na tela com os dados dos menus e barras de ferramentas.
 bool MainApp::OnInit(){
     
      MainFrame *frame = new MainFrame(nameMain, posMain, sizeMain);
+     
+     frame->Show(TRUE);                                 
+     SetTopWindow(frame);
     
     return true;                                  
 };
