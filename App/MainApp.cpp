1c499ac548d1f735fbf9cff36dd87111982d2c17
Tarcisio.Batista <tarcisio.batista@unimontes.br>
2015-06-14 23:53:40 -0300

    comentadas todas as classes e documentação sobre metodos criada

--- App/MainApp.cpp
+++ App/MainApp.cpp
@@ -5,38 +5,43 @@
 
 IMPLEMENT_APP(MainApp)
   
-//O método OnInit cria uma frame na tela com os dados dos menus e barras de ferramentas.
-//Também cria a frame inicial de login no sistema
+//O método OnInit inicia com a splash screen em seguida abre a tela de login.
+//caso o login tenha sucesso o a frame sera aberta de dentro da classe login frame.
 bool MainApp::OnInit(){
     
-    //Frame de login 
+    //colocando o idioma padrão portugues
     SetLanguage(wxLANGUAGE_PORTUGUESE_BRAZILIAN);
+    //Frame de login 
     LoginFrame *lframe = new LoginFrame(_("Login"));
     lframe->Centre();
     lframe->Show(true);
     SplashScreen *SS;
-    SS->SetSS();  
+    SS->SetSS("../../Images/splash.bmp");  
     return true;                                  
 };
 
+//metodo para recriar a aplicação, util pois após o locale ter sido escolhido
+//é necessário recriar a frame.
 void MainApp::RecreateGUI(){
     wxWindow * topwindow = GetTopWindow();
     if(topwindow)
     {
+        //destruindo a janela principal
         SetTopWindow(NULL);
         topwindow->Destroy();
     }
+    //criando nova frame
     MainFrame *frame = new MainFrame(wxT("TP2 - PAC"),posMain, sizeMain);
     frame->Show(true);
     frame->Centre();
 }
 
 void MainApp::SetLanguage(wxLanguage language){
-    // carrega outra lingua se possivel, se nÃ£o volta para a lingua padrÃ£o
      if(wxLocale::IsAvailable(language)){
         
+        //locale recebendo nova linguagem
         locale = new wxLocale(language);
-        // add locale search paths
+        // aicionando os caminos de busca do locale
         locale->AddCatalogLookupPathPrefix(wxT("./Languages"));
         locale->AddCatalogLookupPathPrefix(wxT("../../Languages"));
         locale->AddCatalog(wxT("myapp"));
--- SplashScreen/SplashScreen.cpp
+++ SplashScreen/SplashScreen.cpp
@@ -1,12 +1,15 @@
 #include "SplashScreen.h"
 
-void SplashScreen::SetSS(){
+// a classe SplashScren contem apenas um metodo
+// que recebe o caminho da imagem atraves de uma string
+// e seta a splash screen.
+void SplashScreen::SetSS(wxString img){
     wxInitAllImageHandlers();
     wxBitmap bitmap;
-    if (bitmap.LoadFile("../../Images/splash.bmp",wxBITMAP_TYPE_BMP)){      
+    if (bitmap.LoadFile(img ,wxBITMAP_TYPE_BMP)){      
      wxSplashScreen* splash = new wxSplashScreen(bitmap,
         wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
-        6000, NULL, -1, wxDefaultPosition, wxDefaultSize,
+        3500, NULL, -1, wxDefaultPosition, wxDefaultSize,
         wxBORDER_SIMPLE|wxSTAY_ON_TOP);
     splash->Show(true);
     }    
